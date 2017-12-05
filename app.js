38095729768efed5435482c3ffa28b23efe45904
Chelo QG <cheloq@gmail.com>
2017-12-05 11:28:22 +0100

    commit de lo que tengo en el mac de la presentación de Pillars.js para la betabeers de huelva

--- app.js
+++ app.js
@@ -1,22 +1,123 @@
 //Inclusión de la librería pillars
 var project = require('pillars');
 
-// Configuración del proyecto mediante .configure()
+project.services.get("http").configure({
+	port:3001
+}).start(); 
+
+project.routes.add(new Route({
+	path:"/mytemplate"
+},function(gw){
+	let data = {
+		name: "Manolo",
+		surname: "Gafotas",
+		age: "32"
+	};	
+	gw.render("static/user.hbs", data);
+}));
+
+
+let pillarsDocsStatic = new Route({
+  id:'pillarsDocsStatic',
+  path:'/*:path',
+  directory:{
+    path:'./static',
+    listing:true
+  }
+});
+project.routes.add(pillarsDocsStatic);
+
+
+
+/*
+1.- Hola Mundo
+2.- Configuración del proyecto
+3.- Configuración del servicio
+4.- Creación de un nuevo servicio http en otro puerto
+5.- Configuración de un controlador para que atienda sólo en un puerto determinado
+6.- Incorporación de directorio estático
+7.- Plantilla hbs
+8.- Sesión sin persistencia
+9.- Retransmisión de video en streaming
+10.- Envío de un json
+11.- GET y POST
+12.- Rutas parametrizadas /:var/:*rest
+
+*/
+
+//2.- Configuración del proyecto mediante .configure()
+/*
 project.configure({
 	debug:true, 
 	renderReload: true,
 	cors: false,
-	maxUploadSize: 1*1024*1024
+	maxUploadSize: 1*1024*1024,
+	logFile: false
 });
 
-// Pillars.js incorpora un servicio http built-in, así que tomamos e iniciamos del servicio http, mediante el método .start();
+
+/*
+project.services.get("http").configure({
+	port:3001,
+	timeout: 8000 //Error 408
+}).start();
+
+
+//3.- Pillars.js incorpora un servicio http built-in, así que tomamos e iniciamos del servicio http, mediante el método .start();
 // Para configurar el servicio, se usa el método .configure()
+/*
 project.services.get('http').configure({
 	port:3001,
 	timeout: 8000
 }).start();
 
-// Hola Mundo con las propiedades de route
+//4.- Creación de un nuevo servicio http
+/*project.services.add((new HttpService({
+  id:'http2',
+  port: 3002
+})).start()); 
+
+//5.- Filtrado por un puerto determinado. 
+//Los controladores filtran la petición por: port, path, method, host y https 
+//Ejemplo filtrado de controlador por puerto de petición
+/*
+project.routes.add(new Route({
+	path:"/info",
+	port: 3002
+},gw=>{
+	gw.json(project);
+}));
+
+//6.- Ejemplo directorio estático
+//Añadimos un nuevo controlador que activa el directorio estático (el directorio estático es el middleware directory.js)
+/*
+let pillarsDocsStatic = new Route({
+  id:'pillarsDocsStatic',
+  path:'/*:path',
+  directory:{
+    path:'./static',
+    listing:true
+  }
+});
+project.routes.add(pillarsDocsStatic);
+
+
+//7.- Ejemplo de envío de datos a un template
+/*
+project.routes.add(new Route({
+	path:"/mytemplate"
+},function(gw){
+	let data = {
+		name: "Manolo",
+		surname: "Gafotas",
+		age: "32"
+	};	
+	gw.render("static/user.hbs", data);
+}));
+
+
+//8.- Sesión sin persistencia
+/*
 project.routes.add(new Route({
 	port: 3001,
 	path:"hola",
@@ -32,39 +133,8 @@ project.routes.add(new Route({
 	gw.html("Hola Mundo, esta es tu visita:"+ gw.session.counter);
 }));
 
-
-//Creación de un nuevo servicio http
-project.services.add((new HttpService({
-  id:'http2',
-  port: 3002
-})).start()); 
-
-//Controlador para desactivar el servicio http
-project.routes.add(new Route({
-	path: "off"
-},gw=>{
-	project.services.get("http2").stop();
-	gw.send("Desactivado el servicio http2");
-}));
-
-//Controlador para activa el servicio http2
-project.routes.add(new Route({
-	path: "on"
-},gw=>{
-	project.services.get("http2").start();
-	gw.send("Activado el servicio http2");
-}));
-
-/*Los controladores filtran la petición por: port, path, method, host y https */
-//Ejemplo filtrado de controlador por puerto de petición
-project.routes.add(new Route({
-	path:"/info",
-	port: 3002
-},gw=>{
-	gw.json(project);
-}));
-
-//Ejemplo de envío de un json
+//10.- Ejemplo de envío de un json
+/*
 project.routes.add(new Route({
 	path:"/json"
 },function(gw){
@@ -75,26 +145,9 @@ project.routes.add(new Route({
 		func : function(){console.log("Holita");}
 	};	
 	gw.json(data);
-}));
-
-// Ejemplo de envío de datos a un template
-project.routes.add(new Route({
-	path:"/mytemplate"
-},function(gw){
-	let data = {
-		name: "Manolo",
-		surname: "Gafotas",
-		age: "32"
-	};	
-	gw.render("static/user.hbs", data);
-}));
+}));*/
 
 
-//Ejemplo de video en streaming
-
-
-//Ejemplo de query string, el get de php vaya ?a=2&b="hola"
-/*Los datos del query string están en gw.query*/
 
 
 
@@ -107,14 +160,31 @@ project.routes.add(new Route({
 }));
 */
 
-//Ejemplo directorio estático
-//Añadimos un nuevo controlador que activa el directorio estático (el directorio estático es el middleware directory.js)
-let pillarsDocsStatic = new Route({
-  id:'pillarsDocsStatic',
-  path:'/*:path',
-  directory:{
-    path:'./static',
-    listing:true
-  }
-});
-project.routes.add(pillarsDocsStatic);
+
+
+/*11.- GET*/
+//localhost?a=A&b=B
+//gw.query['a'], gw.query['b']
+//POST
+// El route debe estar configurado en method y multipart a true si vamos a subir archivos
+//gw.files, estarán todos los archivos subidos y almacenados en el directorio temp
+//el contenido del post-> gw.content.params
+
+
+
+//extra.- Controlador para desactivar el servicio http
+/*
+project.routes.add(new Route({
+	path: "off"
+},gw=>{
+	project.services.get("http2").stop();
+	gw.send("Desactivado el servicio http2");
+}));
+
+//Controlador para activa el servicio http2
+/*project.routes.add(new Route({
+	path: "on"
+},gw=>{
+	project.services.get("http2").start();
+	gw.send("Activado el servicio http2");
+}));*/
